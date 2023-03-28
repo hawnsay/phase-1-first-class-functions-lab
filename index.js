d6286c2981806fc16bfc1f1aa596cd5c94624492
Shawn Chestine <shawn@Shawns-MBP.askey.com>
2023-03-27 20:22:57 -0700

    Completes first class functions lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(number) {
+    return function fareMultiplier (createFareMultiplier) {
+        return createFareMultiplier * number;
+    }
+}
+
+function fareDoubler(number) {
+    return number * 2;
+}
+
+function fareTripler(number) {
+    return number * 3;
+}
+
+function selectDifferentDrivers(drivers, functionName) {
+    if (functionName === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers);
+    }
+    if (functionName === returnLastTwoDrivers ) {
+        return returnLastTwoDrivers(drivers);
+    }
+}
